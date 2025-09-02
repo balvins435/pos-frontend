6056ea0ed67063f98db73e13b51ec30e8edfc45a
balvins435 <vincentkiplangat02@gmail.com>
2025-09-02 22:23:05 +0300

    Changed index file

--- src/hooks/useAutoLogouts.ts
+++ src/hooks/useAutoLogouts.ts
@@ -1,21 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 const useAutoLogout = (logoutFn: () => void, timeoutMinutes = 30) => {
   const navigate = useNavigate();
   const logoutTimerRef = useRef<NodeJS.Timeout | null>(null);
 
-  const resetTimer = () => {
+  const resetTimer = useCallback(() => {
     if (logoutTimerRef.current) clearTimeout(logoutTimerRef.current);
     logoutTimerRef.current = setTimeout(() => {
       logoutFn();
       navigate("/login"); // redirect to login
     }, timeoutMinutes * 60 * 1000);
-  };
+  }, [logoutFn, navigate, timeoutMinutes]);
 
   useEffect(() => {
     const events = ["mousemove", "keydown", "click"];
-
     events.forEach((event) => window.addEventListener(event, resetTimer));
 
     resetTimer(); // start timer
@@ -24,8 +23,7 @@ const useAutoLogout = (logoutFn: () => void, timeoutMinutes = 30) => {
       events.forEach((event) => window.removeEventListener(event, resetTimer));
       if (logoutTimerRef.current) clearTimeout(logoutTimerRef.current);
     };
-    // }, [logoutFn, navigate, timeoutMinutes]);
-  }, [logoutFn, navigate, timeoutMinutes]);
+  }, [resetTimer]);
 };
 
 export default useAutoLogout;
